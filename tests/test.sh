7c59e0f9cba582ba23b71ee6976980476190596b
Aloysius Lim <aloysius@abtpeople.com>
2015-05-22 11:03:27 +0800

    Debug Elasticsearch connection on Travis CI.

--- tests/test.sh
+++ tests/test.sh
@@ -3,11 +3,11 @@ DIR=$( dirname $0 )
 set -ev
 
 # Check syntax
-ansible-playbook -i $DIR/inventory -c local --syntax-check -vv $DIR/test.yml
+ansible-playbook -i $DIR/inventory -c local --syntax-check -vvv $DIR/test.yml
 # Check role
-ansible-playbook -i $DIR/inventory -c local --become -vv $DIR/test.yml
+ansible-playbook -i $DIR/inventory -c local --become -vvv $DIR/test.yml
 # Check indempotence
-ansible-playbook -i $DIR/inventory -c local --become -vv $DIR/test_install_latest.yml \
+ansible-playbook -i $DIR/inventory -c local --become -vvv $DIR/test_install_latest.yml \
   | grep -q 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
